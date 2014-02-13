fbe89f05e7c0aea1fbc5108fd5bea5f6694f373a
Ju Liu <ju.liu@welaika.com>
2014-02-13 12:37:04 +0100

    use chars instead of split

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,10 +1,7 @@
 class Hamming
   class << self
     def compute(first, second)
-      first_chars = first.split('')
-      second_chars = second.split('')
-
-      first_chars.zip(second_chars).count do |a, b|
+      first.chars.zip(second.chars).count do |a, b|
         a && b && a != b
       end
     end
