a0afbd41e8e1c8f3289f3b0b5ab2aa95ac2bf830
Ju Liu <ju.liu@welaika.com>
2014-02-14 13:55:50 +0100

    improve performance 500%

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -1,11 +1,17 @@
 class PrimeFactors
   class << self
-    def for(number)
-      lowest_factor = (2..number).detect do |n|
-        number % n == 0
+    def for(number, result = [])
+      return result if number == 1
+
+      lowest_factor = if number.even?
+        2
+      else
+        (3..number/3).step(2).detect do |n|
+          number % n == 0
+        end || number
       end
 
-      lowest_factor ? [lowest_factor] + PrimeFactors.for(number / lowest_factor) : []
+      PrimeFactors.for(number / lowest_factor, result.push(lowest_factor))
     end
   end
 end
